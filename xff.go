5cf8b2597413f2fb7b7d598696814cf442370d1f
Olivier Poitrey <rs@dailymotion.com>
2015-02-19 18:13:25 -0800

    Move the xff parsing logic outside of the handler

--- xff.go
+++ xff.go
@@ -43,37 +43,39 @@ func Parse(ipList string) string {
 	return ""
 }
 
-func parseXFP(port string) string {
-	return port
+// GetRemoteAddr parse the given request and resolve any X-Forwarded-* headers and return
+// the resolved remote address.
+func GetRemoteAddr(r *http.Request) string {
+	xff := r.Header.Get("X-Forwarded-For")
+	xfp := r.Header.Get("X-Forwarded-Port")
+	var ip string
+	if xff != "" {
+		ip = Parse(xff)
+	}
+	var port string
+	if xfp != "" {
+		port = Parse(xfp)
+	}
+	remoteAddr := r.RemoteAddr
+	if ip != "" && port != "" {
+		remoteAddr = fmt.Sprintf("%s:%s", ip, port)
+	} else {
+		oip, oport, err := net.SplitHostPort(r.RemoteAddr)
+		if err == nil {
+			if ip != "" {
+				remoteAddr = fmt.Sprintf("%s:%s", ip, oport)
+			} else if port != "" {
+				remoteAddr = fmt.Sprintf("%s:%s", oip, port)
+			}
+		}
+	}
+	return remoteAddr
 }
 
 // XFF is a middleware to update RemoteAdd from X-Fowarded-* Headers.
 func XFF(h http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
-		xff := r.Header.Get("X-Forwarded-For")
-		xfp := r.Header.Get("X-Forwarded-Port")
-		var ip string
-		if xff != "" {
-			ip = Parse(xff)
-		}
-		var port string
-		if xfp != "" {
-			port = Parse(xfp)
-		}
-		if ip != "" && port != "" {
-			r.RemoteAddr = fmt.Sprintf("%s:%s", ip, port)
-		} else {
-			oip, oport, err := net.SplitHostPort(r.RemoteAddr)
-			if err == nil {
-				if ip != "" {
-					r.RemoteAddr = fmt.Sprintf("%s:%s", ip, oport)
-
-				} else if port != "" {
-					r.RemoteAddr = fmt.Sprintf("%s:%s", oip, port)
-
-				}
-			}
-		}
+		r.RemoteAddr = GetRemoteAddr(r)
 		h.ServeHTTP(w, r)
 	}
 	return http.HandlerFunc(fn)
